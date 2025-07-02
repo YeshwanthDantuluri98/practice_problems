8e7c0022ed49125f5707af4027288a247a609f19
Yeshwanth Dantuluri <69249961+YeshwanthDantuluri98@users.noreply.github.com>
2025-07-03 00:24:19 +0530

    Added some additional changes to the code
    Added some additional changes to the code

--- missingNumber.py
+++ missingNumber.py
@@ -1,10 +1,17 @@
+"""
+Write a function to find the missing number in a given integer array of 1 to 100.
+The function takes to parameter the array and the number of elements that needs to be in array.
+For example if we want to find missing number from 1 to 6 the second parameter will be 6.
+
+For Example missing_number([1,2,3,4,6],6) Answer would be 5
+"""
+
 def missing_number(arr,n):
     temp = []
     for i in range(n):
         temp.append(i+1)
-    print(temp)
-    getMissingNumber = list(set(temp) - set(arr))
-    return getMissingNumber[0]
+    missing_number = list(set(temp) - set(arr))
+    return missing_number[0]
 
 
 missing_number([1,2,3,4,6],6)
@@ -13,6 +20,6 @@ def missing_number_2(arr, n):
     total = n * (n+1) // 2
     sum_arr = sum(arr)
     missing_number = total - sum_arr
-    print(missing_number)
+    return missing_number
 
 missing_number_2([1,2,3,4,5],6)
